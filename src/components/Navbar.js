4d88a717dd201a4eb18ebcf592b61efc4f623bf7
AbhayJayani <abhayjayani2003@gmail.com>
2024-07-24 17:05:11 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4 sticky top-0 z-50">
+      <div className="container mx-auto">
+        <h1 className="text-white text-center text-3xl">Image Gallery</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
