ac78aac973f3f8484214ba5344abc862f0f40412
AbhayJayani <abhayjayani2003@gmail.com>
2024-07-28 18:03:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
 import Navbar from './components/Navbar';
+import NavbarButtons from './components/NavbarButtons';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -20,19 +21,20 @@ function App() {
 
   return (
     <div>
-      <Navbar />
-      <div className="container mx-auto">
-      
-      <ImageSearch searchText={(text) => setTerm(text)} />
-
-      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
-
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>}
-    </div>
+      <Navbar searchText={(text) => setTerm(text)} />
+      <NavbarButtons />
+      <div className="container mx-auto mt-4">
+        {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+        {isLoading ? (
+          <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+        ) : (
+          <div className="grid grid-cols-3 gap-4">
+            {images.map(image => (
+              <ImageCard key={image.id} image={image} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
